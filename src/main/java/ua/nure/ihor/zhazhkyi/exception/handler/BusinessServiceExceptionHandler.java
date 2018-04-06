99485cf41914271c19422bb5254d34e945ac50e9
Ihor Zhazhkyi <Ihor_Zhazhkyi@epam.com>
2018-04-06 19:06:07 +0300

    Added correct error msg and registration check for user

--- src/main/java/ua/nure/ihor/zhazhkyi/exception/handler/BusinessServiceExceptionHandler.java
+++ src/main/java/ua/nure/ihor/zhazhkyi/exception/handler/BusinessServiceExceptionHandler.java
@@ -1,13 +1,11 @@
-package ua.nure.ihor.zhazhkyi.exception;
+package ua.nure.ihor.zhazhkyi.exception.handler;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ua.nure.ihor.zhazhkyi.exception.ExceptionResponseBody;
 
 @ControllerAdvice
 public class BusinessServiceExceptionHandler {
